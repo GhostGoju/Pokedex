8cb5566fb8da681692bdb6f1d6017e66269f7df7
GhostGoju <129934609+GhostGoju@users.noreply.github.com>
2024-03-18 09:53:40 -0500

    act

--- Backend/db/mysql.php
+++ Backend/db/mysql.php
@@ -5,7 +5,7 @@ class BaseDeDatos
 
     public function __construct()
     {
-        $this->conexion = new mysqli("localhost", "root", "Qwe.proyectoSena", "Pokedex");
+        $this->conexion = new mysqli("localhost", "root", "", "Pokedex");
 
         if ($this->conexion->connect_error) {
             die("Connection failed: " . $this->conexion->connect_error);
